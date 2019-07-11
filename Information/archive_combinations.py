2f4dadd02c13ca758d7b862d873923c77d706295
Oliver Calder <caldero@carleton.edu>
2019-07-11 17:44:04 -0500

    Scripted combinations and fixed problems

---
+++ Information/archive_combinations.py
@@ -0,0 +1,98 @@
+'''
+archive_combinations.py
+
+Used to generate Archive directory and run baseline accuracy comparisons
+'''
+
+from itertools import combinations
+
+
+defaults = [[{'silent':True},'-s'], [{'wt':True},'-wt'] [{'wj':True},'-wj'], [{'cascade':True},'-R']]
+
+directory = [{'directory':'../Archive/'},'-d ../Archive/']
+
+char_combos = [
+        [{},                 '',        'All'      ], 
+        [{'min_words':100},  '-m 100',  'Min-100'  ], 
+        [{'min_words':250},  '-m 250',  'Min-250'  ], 
+        [{'min_words':500},  '-m 500',  'Min-500'  ], 
+        [{'min_words':1000}, '-m 1000', 'Min-1000' ], 
+        [{'min_words':2500}, '-m 2500', 'Min-2500' ], 
+        [{'eo':True},        '-eo',     'No-Others']]
+
+
+options = [[{'preserve_emphasis':True},'-e','Emphasis'], [{'vowels_only':True},'v','Vowels-Only']]
+def get_option_combos():
+    combos = []
+    for i in range(len(options) + 1):
+        combo_list = combinations(options, i)
+        for combo in combo_list:
+            if len(combo) > 0:
+                combo = sorted(combo, key=lambda c: c[2])
+            combos.append(list(combo))
+    return combos
+
+
+def get_param_dicts():
+    param_dicts = []
+    for char_combo in char_combos:
+        char_param = char_combo[0]
+        for option_combo in get_option_combos():
+            param_dict = {}
+            for item in defaults:
+                param_dict.update(item[0])
+            param_dict.update(char_param)
+
+            dir_list = []
+            for option in option_combo:
+                param_dict.update(option[0])
+                dir_list.append(option[2])
+            dir_list.append(char_combo[2])
+            dir_param = directory[0]['directory'] + '-'.join(dir_list)
+            param_dict['directory'] = dir_param
+
+            param_dicts.append(param_dict)
+    return param_dicts
+
+
+def get_arg_strings():
+    arg_strings = []
+    for char_combo in char_combos:
+        char_arg = char_combo[1]
+        for option_combo in get_option_combos():
+            arg_list = []
+            arg_list += [arg[1] for arg in defaults]
+            arg_list.append(char_arg)
+            
+            dir_list = []
+            for option in option_combo:
+                arg_list.append(option[1])
+                dir_list.append(option[2])
+            dir_list.append(char_combo[2])
+            dir_arg = directory[1] + '-'.join(dir_list)
+            arg_list.append(dir_string)
+
+            arg_string = arg_list.join(' ')
+            arg_strings.append(arg_string)
+    return arg_strings
+
+
+def test():
+    combos = get_option_combos()
+    print('get_option_combos():')
+    for item in combos:
+        print(item)
+    print()
+    params = get_param_dicts()
+    print('get_param_dicts()')
+    for item in params:
+        print(item)
+    print()
+    args = get_arg_strings()
+    print('get_arg_strings()')
+    for item in args:
+        print(item)
+
+
+if __name__ == '__main__':
+    test()
---
+++ Preprocessing/load_counts.safe_generate_Archive.py
@@ -0,0 +1,104 @@
+import sys
+import queue
+import threading
+import percentages
+import time
+
+
+class MyThread(threading.Thread):
+    def __init__(self, thread_name, work_queue, queue_lock, exit_flag):
+        threading.Thread.__init__(self)
+        self.thread_name = thread_name
+        self.work_queue = work_queue
+        self.queue_lock = queue_lock
+        self.exit_flag = exit_flag
+
+    def run(self):
+        print('Starting', self.thread_name)
+        build_directory(self.thread_name, self.work_queue, self.queue_lock, self.exit_flag)
+        print('Exiting', self.thread_name)
+
+
+def build_directory(thread_name, work_queue, queue_lock, exit_flag):
+    while not exit_flag[0]:
+        queue_lock.acquire()
+        if not work_queue.empty():
+            name, args_dict = work_queue.get()
+            queue_lock.release()
+            print(thread_name, 'beginning', name)
+            percentages.build_percentages(**args_dict)  # Way to unpack args_dict and assign to appropriate optional parameters
+            print(thread_name, 'finished', name)
+        else:
+            queue_lock.release()
+            time.sleep(1)
+
+def main(thread_count):
+    combinations = [
+        ('Emphasis-All', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-All/', 'load_json_filenames':['../Archive/Emphasis-All/counts.json'], 'cascade':True, 'preserve_emphasis':True}),
+        ('Emphasis-Vowels-Only-All', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-Vowels-Only-All/', 'load_json_filenames':['../Archive/Emphasis-Vowels-Only-All/counts.json'], 'cascade':True, 'vowels_only':True, 'preserve_emphasis':True}),
+        ('All', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/All/', 'load_json_filenames':['../Archive/All/counts.json'], 'cascade':True}),
+        ('Vowels-Only-All', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Vowels-Only-All/', 'load_json_filenames':['../Archive/Vowels-Only-All/counts.json'], 'cascade':True, 'vowels_only':True}),
+        ('Emphasis-Min-100', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-Min-100/', 'load_json_filenames':['../Archive/Emphasis-Min-100/counts.json'], 'cascade':True, 'min_words':100, 'preserve_emphasis':True}),
+        ('Emphasis-Vowels-Only-Min-100', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-Vowels-Only-Min-100/', 'load_json_filenames':['../Archive/Emphasis-Vowels-Only-Min-100/counts.json'], 'cascade':True, 'min_words':100, 'vowels_only':True, 'preserve_emphasis':True}),
+        ('Min-100', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Min-100/', 'load_json_filenames':['../Archive/Min-100/counts.json'], 'cascade':True, 'min_words':100}),
+        ('Vowels-Only-Min-100', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Vowels-Only-Min-100/', 'load_json_filenames':['../Archive/Vowels-Only-Min-100/counts.json'], 'cascade':True, 'min_words':100, 'vowels_only':True}),
+        ('Emphasis-Min-250', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-Min-250/', 'load_json_filenames':['../Archive/Emphasis-Min-250/counts.json'], 'cascade':True, 'min_words':250, 'preserve_emphasis':True}),
+        ('Emphasis-Vowels-Only-Min-250', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-Vowels-Only-Min-250/', 'load_json_filenames':['../Archive/Emphasis-Vowels-Only-Min-250/counts.json'], 'cascade':True, 'min_words':250, 'vowels_only':True, 'preserve_emphasis':True}),
+        ('Min-250', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Min-250/', 'load_json_filenames':['../Archive/Min-250/counts.json'], 'cascade':True, 'min_words':250}),
+        ('Vowels-Only-Min-250', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Vowels-Only-Min-250/', 'load_json_filenames':['../Archive/Vowels-Only-Min-250/counts.json'], 'cascade':True, 'min_words':250, 'vowels_only':True}),
+        ('Emphasis-Min-500', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-Min-500/', 'load_json_filenames':['../Archive/Emphasis-Min-500/counts.json'], 'cascade':True, 'min_words':500, 'preserve_emphasis':True}),
+        ('Emphasis-Vowels-Only-Min-500', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-Vowels-Only-Min-500/', 'load_json_filenames':['../Archive/Emphasis-Vowels-Only-Min-500/counts.json'], 'cascade':True, 'min_words':500, 'vowels_only':True, 'preserve_emphasis':True}),
+        ('Min-500', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Min-500/', 'load_json_filenames':['../Archive/Min-500/counts.json'], 'cascade':True, 'min_words':500}),
+        ('Vowels-Only-Min-500', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Vowels-Only-Min-500/', 'load_json_filenames':['../Archive/Vowels-Only-Min-500/counts.json'], 'cascade':True, 'min_words':500, 'vowels_only':True}),
+        ('Emphasis-Min-1000', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-Min-1000/', 'load_json_filenames':['../Archive/Emphasis-Min-1000/counts.json'], 'cascade':True, 'min_words':1000, 'preserve_emphasis':True}),
+        ('Emphasis-Vowels-Only-Min-1000', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-Vowels-Only-Min-1000/', 'load_json_filenames':['../Archive/Emphasis-Vowels-Only-Min-1000/counts.json'], 'cascade':True, 'min_words':1000, 'vowels_only':True, 'preserve_emphasis':True}),
+        ('Min-1000', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Min-1000/', 'load_json_filenames':['../Archive/Min-1000/counts.json'], 'cascade':True, 'min_words':1000}),
+        ('Vowels-Only-Min-1000', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Vowels-Only-Min-1000/', 'load_json_filenames':['../Archive/Vowels-Only-Min-1000/counts.json'], 'cascade':True, 'min_words':1000, 'vowels_only':True}),
+        ('Emphasis-Min-2500', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-Min-2500/', 'load_json_filenames':['../Archive/Emphasis-Min-2500/counts.json'], 'cascade':True, 'min_words':2500, 'preserve_emphasis':True}),
+        ('Emphasis-Vowels-Only-Min-2500', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-Vowels-Only-Min-2500/', 'load_json_filenames':['../Archive/Emphasis-Vowels-Only-Min-2500/counts.json'], 'cascade':True, 'min_words':2500, 'vowels_only':True, 'preserve_emphasis':True}),
+        ('Min-2500', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Min-2500/', 'load_json_filenames':['../Archive/Min-2500/counts.json'], 'cascade':True, 'min_words':2500}),
+        ('Vowels-Only-Min-2500', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Vowels-Only-Min-2500/', 'load_json_filenames':['../Archive/Vowels-Only-Min-2500/counts.json'], 'cascade':True, 'min_words':2500, 'vowels_only':True}),
+        ('Emphasis-No-Others', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-No-Others/', 'load_json_filenames':['../Archive/Emphasis-No-Others/counts.json'], 'cascade':True, 'eo':True, 'preserve_emphasis':True}),
+        ('Emphasis-Vowels-Only-No-Others', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Emphasis-Vowels-Only-No-Others/', 'load_json_filenames':['../Archive/Emphasis-Vowels-Only-No-Others/counts.json'], 'cascade':True, 'eo':True, 'vowels_only':True, 'preserve_emphasis':True}),
+        ('No-Others', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/No-Others/', 'load_json_filenames':['../Archive/No-Others/counts.json'], 'cascade':True, 'eo':True}),
+        ('Vowels-Only-No-Others', {'silent':True, 'wt':True, 'wj':True, 'directory':'../Archive/Vowels-Only-No-Others/', 'load_json_filenames':['../Archive/Vowels-Only-No-Others/counts.json'], 'cascade':True, 'eo':True, 'vowels_only':True})
+        ]
+    
+    exit_flag = [0]
+    work_queue = queue.Queue(len(combinations))
+    queue_lock = threading.Lock()
+    
+    # Create threads
+    threads = []
+    for i in range(int(thread_count)):
+        thread_name = 'Thread_{}'.format(i)
+        thread = MyThread(thread_name, work_queue, queue_lock, exit_flag)
+        thread.start()
+        threads.append(thread)
+
+    # Fill the queue
+    queue_lock.acquire()
+    for item in combinations:
+        work_queue.put(item)
+    queue_lock.release()
+
+    # Wait for queue to empty
+    while not work_queue.empty():
+        time.sleep(1)
+
+    # Notify threads of end
+    exit_flag[0] = 1
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+    print('Exiting Main Thread')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python3 {} #_of_threads'.format(sys.argv[0]))
+        quit()
+    else:
+        thread_count = sys.argv[1]
+        main(thread_count)
