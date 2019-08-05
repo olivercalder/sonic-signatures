6d702669f7f5e81b0f09b1ace7f4b8ec23639c46
Oliver Calder <oacalder@gmail.com>
2019-08-05 17:27:39 -0500

    Added persistent label on click, and implemented brushing for all characters of the same name across charts

--- Visualization/beeswarm.js
+++ Visualization/beeswarm.js
@@ -23,53 +23,61 @@ function getColor(d, classifier) {
 
 function colorCircles() {
     d3.selectAll("circle")
+        .transition()
+        .duration(1000)
         .style("fill", function(d) { return getColor(d, classifier) })
         .style("stroke", function(d) { return getColor(d, classifier) });
 };
 
-function handleMouseOver(d, i) {
-    // d3.selectAll("." + d.character);
-    let circ = d3.select(this)  // circle element being modified
-        .style("fill", "white")
-        .style("stroke", "black");
+function handleClick(d) {
+    let circ = d3.select(this);  //circle element triggering event
 
-    let charName = d.character;
-    let Zscore = d.Zscore.toFixed(3);
+    let newPersist = (parseInt(circ.attr("persist")) + 1) % 2;
 
-    let graph = d3.select(this.parentNode)  // g element containing circles
-
-    let chart = d3.select(this.parentNode.parentNode)  // svg element containing everything
-
-    let chartID = chart.attr("id");
-    let phonIndex = chartID.indexOf("_");
-    let phon = chartID.slice(phonIndex + 1);
-
-    let labelID = phon + "_" + charName.replace(/\./g, "-") + "_label";
-
-    circLabel = graph.append("text")
-        .attr("id", labelID)
-        .attr("x", function() { return circ.attr("cx") - 50 })
-        .attr("y", function() { return circ.attr("cy") - 15 })
-        .text(function() { return charName + ": " + Zscore; });
+    d3.selectAll(".c" + d.character.replace(/\./g, "-"))  // all circle elements for the same character
+        .attr("persist", function() { return newPersist; });
 }
 
-function handleMouseOut(d, i) {
-    circ = d3.select(this)
-        .style("fill", function() { return getColor(d, classifier) })
-        .style("stroke", function() { return getColor(d, classifier) });
+function handleMouseOver(d) {
+    let circ = d3.select(this);  // circle element triggering event
+    if (circ.attr("persist") == 0) {
 
-    let graph = d3.select(this.parentNode)  // g element containing circles
+        d3.selectAll("circle.c" + d.character.replace(/\./g, "-"))  // all circle elements for the same character
+            .transition()
+            .duration(100)
+            .attr("r", function() { return circ.attr("r") * 2; })
+            .style("stroke", "white")
+            .each(function(element) {
+                let currentCirc = d3.select(this);
+                let phon = currentCirc.attr("phoneme");
 
-    let chart = d3.select(this.parentNode.parentNode)  // svg element containing everything
+                let graph = d3.select(this.parentNode);  // g element containing circles for each circle
 
-    let chartID = chart.attr("id");
-    let phonIndex = chartID.indexOf("_");
-    let phon = chartID.slice(phonIndex + 1);
-    let charName = circ.attr("character");
+                circLabel = graph.append("text")
+                    .attr("class", function() { return "c" + d.character.replace(/\./g, "-") + " popup"; })
+                    .attr("x", function() { return currentCirc.attr("cx") - 50 })
+                    .attr("y", function() { return currentCirc.attr("cy") - 15 })
+                    .text(function() { return d.character + ": " + parseFloat(currentCirc.attr("Zscore")).toFixed(3); });
+                }
+            )
+    }
+}
 
-    let labelID = phon + "_" + charName.replace(/\./g, "-") + "_label";
+function handleMouseOut(d) {
+    let circ = d3.select(this);  // circle element triggering event
 
-    d3.select("#" + labelID).remove();
+    if (circ.attr("persist") == 0) {
+
+        d3.selectAll("circle.c" + d.character.replace(/\./g, "-"))  // all circle elements for the same character
+            .transition()
+            .duration(100)
+            .attr("r", function() { return radius; })
+            .style("stroke", function() { return getColor(d, classifier) });
+
+        let chart = d3.select(this.parentNode.parentNode);  // svg element containing everything
+
+        d3.selectAll("text.c" + d.character.replace(/\./g, "-") + ".popup").remove();
+    }
 }
 
 
@@ -117,20 +125,22 @@ d3.json("https://raw.githubusercontent.com/olivercalder/sonic-signatures/master/
         var svg = svgs.enter().append("svg")
             .attr("width", width)
             .attr("height", height)
-            .attr("id", function(d) { return "Chart_" + d.phoneme; });
+            .attr("id", function(d) { return "Chart_" + d.phoneme; })
+            .attr("class", function(d) { return d.phoneme; });
 
         svg.each(function(chartEntry) {
             phoneme = chartEntry.phoneme;
             chartData = chartEntry.data;
 
             var label = d3.select(this).append("g")  // Element to contain phoneme label
-                .attr("class", "label")
+                .attr("class", function() { return phoneme; })
                 .attr("width", xMargin)
                 .attr("height", height);
 
             label.append("text")  // Text within the Label element
                 .attr("x", xMargin / 2)
                 .attr("y", height / 2)
+                .attr("class", function() { return phoneme + " label"; })
                 .style("text-anchor", "middle")
                 .style("fill", "#999999")
                 .style("stroke", "black")
@@ -138,7 +148,7 @@ d3.json("https://raw.githubusercontent.com/olivercalder/sonic-signatures/master/
                 .text(phoneme);
 
             var legend = d3.select(this).append("g")  // Element to contain legend for classes
-                .attr("class", "legend")
+                .attr("class", function() { return phoneme + " legend"; })
                 .attr("transform", "translate(" + xMargin + ", 0)")
                 .attr("width", width - xMargin)
                 .attr("height", yMargin);
@@ -165,7 +175,7 @@ d3.json("https://raw.githubusercontent.com/olivercalder/sonic-signatures/master/
             var xAxis = d3.axisBottom(xScale);
 
             var axis = d3.select(this).append("g")  // Element to contain axis
-                .attr("class", "axis")
+                .attr("class", function() { return phoneme + " axis"; })
                 .attr("transform", "translate(" + xMargin + "," + (height - (yMargin / 2)) + ")")
                 .attr("width", width - xMargin)
                 .attr("height", yMargin)
@@ -191,7 +201,7 @@ d3.json("https://raw.githubusercontent.com/olivercalder/sonic-signatures/master/
             for (let i = 0; i < 120; i++) { simulation.tick(); };
 
             var g = d3.select(this).append("g")  // Element to contain circles
-                .attr("class", "graph")
+                .attr("class", function() { return phoneme + " graph"; })
                 .attr("transform", "translate(" + xMargin + "," + yMargin + ")")
                 .attr("width", width - xMargin)
                 .attr("height", height - yMargin);
@@ -202,15 +212,21 @@ d3.json("https://raw.githubusercontent.com/olivercalder/sonic-signatures/master/
                 .attr("r", function(d) { return radius; })
                 .attr("cx", function(d) { return d.x; })  //truncScore(d.Zscore)); })
                 .attr("cy", function(d) { return d.y; })
+                .attr("phoneme", phoneme)
                 .attr("character", function(d) { return d.character; })
                 .attr("Zscore", function(d) { return d.Zscore; })
                 .attr("title", function(d) { return d.character + ": " + d.Zscore.toFixed(3); })
+                .attr("persist", 0)
                 .on("mouseover", handleMouseOver)
-                .on("mouseout", handleMouseOut);
+                .on("mouseout", handleMouseOut)
+                .on("click", handleClick);
 
             let classifier_keys = Object.keys(classifiers);
             circle.attr("class", function(d) { 
                 let class_list = new Array();
+                class_list.push("c" + d.character.replace(/\./g, "-"));
+                class_list.push("p" + d.character.split("_")[0]);
+                class_list.push(phoneme);
                 for (let i = 0; i < classifier_keys.length; i++) {
                     class_list.push(d[classifier_keys[i]]);
                 };
